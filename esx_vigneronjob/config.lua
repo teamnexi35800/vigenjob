445976e507e7863adc7df1f4242d206d2bd7cfe2
NEXI <80526507+teamnexi35800@users.noreply.github.com>
2021-06-23 22:38:59 +0200

    Initial commit

---
+++ esx_vigneronjob/config.lua
@@ -0,0 +1,75 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MaxInService               = -1
+Config.EnablePlayerManagement     = true
+Config.EnableSocietyOwnedVehicles = false
+Config.Locale                     = 'fr'
+
+Config.Zones = {
+
+	RaisinFarm = {
+		Pos   = {x = -1809.662, y = 2210.119, z = 90.681},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Récolte de raisin",
+		Type  = 1
+	},
+
+	TraitementVin = {
+		Pos   = {x = -53.116, y = 1903.248, z = 194.361},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Traitement du Vin",
+		Type  = 1
+	},
+
+	TraitementJus = {
+		Pos   = {x = 811.337, y = 2179.402, z = 51.388},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Traitement du Jus de raisin",
+		Type  = 1
+	},
+	
+	SellFarm = {
+		Pos   = {x = 3332.24, y = 5159.15, z = 18.57},
+		Size  = {x = 1.5, y = 1.5, z = 1.5},
+		Color = {r = 255, g = 0, b = 0},
+		Name  = "Vente des produits",
+		Type  = 29
+	},
+
+	VigneronActions = {
+		Pos   = {x = -1876.03, y = 2061.13, z = 145.57},
+		Size  = {x = 1.9, y = 2.4, z = 0.5},
+		Color = {r = 136, g = 43, b = 216},
+		Name  = "Point d'action",
+		Type  = 1
+	 },
+	  
+	VehicleSpawner = {
+		Pos   = {x = -1885.56, y = 2057.80, z = 141.00},
+		Size = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 144, g = 255, b = 0},
+		Name  = "Garage véhicule",
+		Type  = 39
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = -1888.79, y = 2046.89, z = 140.75},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Spawn point",
+		Type  = -1
+	},
+
+	VehicleDeleter = {
+		Pos   = {x = -1925.08, y = 2064.72, z = 140.20},
+		Size  = {x = 3.0, y = 3.0, z = 1.5},
+		Color = {r = 255, g = 44, b = 0},
+		Name  = "Ranger son véhicule",
+		Type  = 39
+	}
+
+}
+
